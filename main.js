f3d91394dc065bcac65f2d7108aa0f22936ca6e7
Chubilu01 <5117.joaopedro@gmail.com>
2022-05-17 23:52:15 -0300

    just changed the function name

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ background = ['#292929', '#F2F2F2', '#C7C7C7'];
 text = ['Background:  #292929', 'Background:  #F2F2F2', 'Background:  #C7C7C7'];
 clickNumber = 0;
 
-const change = () => {
+const mouseClick = () => {
     document.body.style.backgroundColor = background[(clickNumber)%3];
     document.querySelector('.hex-code').innerHTML = text[(clickNumber)%3];
     clickNumber++;
 }
 
-change();
+mouseClick();
