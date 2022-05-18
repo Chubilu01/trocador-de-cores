668e2ed2020f32363428b76d6fcd8050f294b22a
Chubilu01 <5117.joaopedro@gmail.com>
2022-05-18 17:20:35 -0300

    polished the js code

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-background = ['#292929', '#F2F2F2', '#C7C7C7'];
-text = ['Background:  #292929', 'Background:  #F2F2F2', 'Background:  #C7C7C7'];
-clickNumber = 0;
+const background = ['#292929', '#F2F2F2', '#C7C7C7'];
+const text = ['Background:  #292929', 'Background:  #F2F2F2', 'Background:  #C7C7C7'];
+let clickNumber = 0;
 
 const mouseClick = () => {
-    document.body.style.backgroundColor = background[(clickNumber)%3];
-    document.querySelector('.hex-code').innerHTML = text[(clickNumber)%3];
+    document.body.style.backgroundColor = background[clickNumber];
+    document.querySelector('.hex-code').innerHTML = text[clickNumber];
     clickNumber++;
+    if (clickNumber == 3) {
+        clickNumber = 0;
+    }
 }
 
 mouseClick();
